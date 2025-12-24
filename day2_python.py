6ca21e817325dc6ca60c7c5621f5473c03b59537
Taha-12345 <164293775+Taha-12345@users.noreply.github.com>
2025-12-25 02:38:11 +0500

    Add files via upload

---
+++ day2_python.py
@@ -0,0 +1,24 @@
+name = "Muhammad Taha"
+age = 21
+marks = 90.89
+
+print(type(name))
+print(type(age))
+print(type(marks))
+
+a = str(input("What is your name?"))
+print(a)
+
+#Store your age in a variable and print it
+age = 21
+print(age)
+
+#Take user name as input and print a welcome message
+NAME = str(input("What is your name?"))
+print("Hi!", NAME ,",Welcome to Python journey")
+
+#Take two numbers from user and print their sum
+b = int(input("Enter first number: "))
+c = int(input("Enter second number: "))
+sum = b + c
+print(sum)
